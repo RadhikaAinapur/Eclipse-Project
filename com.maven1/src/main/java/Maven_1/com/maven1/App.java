e3f1274727b56a6e76534f01fd7a0c3fb1f4ed41
RadhikaAinapur <ainapurradhika@gmail.com>
2024-02-21 12:07:39 +0530

    Commit in Eclipse project

---
+++ com.maven1/src/main/java/Maven_1/com/maven1/App.java
@@ -0,0 +1,13 @@
+package Maven_1.com.maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Git!" );
+    }
+}
